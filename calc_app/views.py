d7f3ebd56223be9ca6e0d640a547d023b07f17e8
shareef829 <shareefshaik97626@gmail.com>
2024-08-28 20:19:34 +0530

    Update views.py

--- calc_app/views.py
+++ calc_app/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def calc(req):
     if req.method == "POST":
-        first = req.POST["first"]
-        second = req.POST["second"]
-        result = int(first) + int(second)        
+        try:
+            first = int(req.POST["first"])
+            second = int(req.POST["second"])
+            result = first + second
+        except (ValueError, KeyError):
+            first = second = result = "Invalid input"
     else:
-        first = 0 
-        second = 0 
-        result = 0 
-    return render(req,'calc.html',{'first':first,
-                                   'second':second,
-                                   'result':result})
+        first = second = result = 0
+    
+    return render(req, 'calc.html', {
+        'first': first,
+        'second': second,
+        'result': result
+    })
